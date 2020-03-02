059a614f29864f32d6ec328a0acc3bd8dd3fae96
jasonit8 <52004170+jasonit8@users.noreply.github.com>
2020-03-02 14:50:21 +0700

    thay đổi file xuất
    Theo đề nghị của Thức, anh vẫn cho chương trình đọc index.html gốc nhưng xuất ra index_newfooter.html.

--- python/footer.py
+++ python/footer.py
@@ -8,7 +8,7 @@ class footer():
         self.soup = BeautifulSoup(f.read(),features='lxml')
         f.close()
     def change_image(self,url='img/logo-light.png'):
-        f = open('../index.html',mode='w',encoding='utf8')
+        f = open('../index_newfooter.html',mode='w',encoding='utf8')
         self.soup.find(id='footer-image').findChildren()[0]['src'] = url
         f.write(str(self.soup.prettify()))
         f.close()
@@ -20,14 +20,14 @@ class footer():
         city: thành phố
         phone: số điện thoại
         '''
-        f = open('../index.html',mode='w',encoding='utf8')
+        f = open('../index_newfooter.html',mode='w',encoding='utf8')
         self.soup.find_all(attrs={'class':'address-city'})[column-1].string = city
         self.soup.find_all(attrs={'class':'address-phone'})[column-1].contents[0].replace_with('%s, %s, %s' %(number,street,city))
         self.soup.find_all(attrs={'class':'address-phone'})[column-1].contents[1].string = phone
         f.write(str(self.soup.prettify()))
         f.close()
     def change_copyright(self,copy_right='© copyright 2020. All Rights Reserved.'):
-        f = open('../index.html',mode='w',encoding='utf8')
+        f = open('../index_newfooter.html',mode='w',encoding='utf8')
         self.soup.find(id='copyright').string = copy_right
         f.write(str(self.soup.prettify()))
         f.close()
