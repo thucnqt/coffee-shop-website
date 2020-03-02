6a2eea6426ba5ece69e90a055eccc70786de6351
jasonit8 <52004170+jasonit8@users.noreply.github.com>
2020-03-02 17:05:21 +0700

    thêm hàm to_html để tinh gọn lớp

--- python/footer.py
+++ python/footer.py
@@ -1,17 +1,18 @@
 if __name__ == "__main__":
     from bs4 import BeautifulSoup
 
-
 class footer():
     def __init__(self):
-        f = open('../index.html',mode='r',encoding='utf8')
+        f = open('../index_newfooter.html',mode='r',encoding='utf8')
         self.soup = BeautifulSoup(f.read(),features='lxml')
         f.close()
-    def change_image(self,url='img/logo-light.png'):
+    def to_html(self,change):
         f = open('../index_newfooter.html',mode='w',encoding='utf8')
-        self.soup.find(id='footer-image').findChildren()[0]['src'] = url
-        f.write(str(self.soup.prettify()))
+        f.write(change)
         f.close()
+    def change_image(self,url='img/logo-light.png'):
+        self.soup.find(id='footer-image').findChildren()[0]['src'] = url
+        self.to_html(str(self.soup.prettify()))
     def change_address(self,column,number,street,city,phone):
         '''
         column: cột muốn sửa, điền 1, 2 hoặc 3
@@ -20,17 +21,13 @@ class footer():
         city: thành phố
         phone: số điện thoại
         '''
-        f = open('../index_newfooter.html',mode='w',encoding='utf8')
         self.soup.find_all(attrs={'class':'address-city'})[column-1].string = city
         self.soup.find_all(attrs={'class':'address-phone'})[column-1].contents[0].replace_with('%s, %s, %s' %(number,street,city))
         self.soup.find_all(attrs={'class':'address-phone'})[column-1].contents[1].string = phone
-        f.write(str(self.soup.prettify()))
-        f.close()
+        self.to_html(str(self.soup.prettify()))
     def change_copyright(self,copy_right='© copyright 2020. All Rights Reserved.'):
-        f = open('../index_newfooter.html',mode='w',encoding='utf8')
         self.soup.find(id='copyright').string = copy_right
-        f.write(str(self.soup.prettify()))
-        f.close()
+        self.to_html(str(self.soup.prettify()))
 
 # ví dụ
 ft = footer()
