b2dd5a320a113433db4c358026092f9e45b2d10f
Knowledge <thucnqt@gmail.com>
2020-03-05 11:22:37 +0700

    updated comment

--- python/Gen.py
+++ python/Gen.py
@@ -1,8 +1,14 @@
  # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import os.path
+
+# Tạo class: Mở  File, Sau đó đọc với thư viện BeautifulSoup.
+# Ghi đè những nội dung thay đổi
 class create():
     def __init__(self):
+        # Kiểm tra file index_new.html (là file được generate ra) 
+        # nếu tồn tại tiếp tục đọc và ghi đè thông tin cần thay đổi.
+        # k có file đó thì đọc và ghi lên 1 file mới.
         if os.path.isfile('../index_new.html'):
             f = open('../index_new.html',encoding='utf8',mode='r')
             self.soup = BeautifulSoup(f.read(),features='lxml')
@@ -15,6 +21,8 @@ class create():
         f = open('../index_new.html',encoding='utf8',mode='w')
         f.write(change)
         f.close()
+
+# Tạo class banner: thay đội nội dung phần slide, kế thừa lớp create
 class banner(create):
     def change_text(self,slide,line,text):
         # slide: điền trang muốn thay đổi; 1 hoặc 2
@@ -23,6 +31,8 @@ class banner(create):
         self.soup.find(id=[['bannertext1','bannertitle1','bannertyping1'],
                            ['bannertext2','bannertitle2','bannertyping2']][slide-1][line-1]).string.replace_with(text)
         self.to_html(str(self.soup.prettify()))
+
+# Tạo class headers: thay đổi nội dung phần navbar, kế thừa lớp create
 class header(create):
     def change_items(self,items):
         for item in range(8): # navbar có 8 mục không đổi
@@ -32,11 +42,15 @@ class header(create):
         self.soup.find(id='logo-img')['src'] = url
         self.to_html(str(self.soup.prettify()))
 
+# Tạo class photo: thay đổi  hình ảnh phần photo, kế thừa lớp create
 class photo(create):
     def change_image(self,item_n,img_after_click='img/coffee1.jpg',img_page='img/coffee1.jpg'):
         self.soup.find_all(attrs={'class':'item-photo'})[item_n-1].findChildren()[0]['href'] = img_after_click
         self.soup.find_all(attrs={'class':'item-photo'})[item_n-1].findChildren()[1]['src'] = img_page
         self.to_html(str(self.soup.prettify()))
+
+# Tạo class New: thay đổi nội dung phần News, kế thừa lớp create
+
 class New(create):
     def change_main(self,head='New & photo',detail ='Cap nhat nhung tin tuc nong hoi'):
         self.soup.find_all(attrs={'class':'first_news'})[0].string = head
@@ -52,6 +66,7 @@ class New(create):
         self.soup.find_all(attrs={'class':'button_news'})[0].string = button_name
         self.to_html(str(self.soup.prettify()))
 
+# Tạo class footer: thay đổi nội dung phần footer, kế thừa lớp create
 class footer(create):
     def change_image(self,url='img/logo-light.png'):
         self.soup.find(id='footer-image').findChildren()[0]['src'] = url
@@ -72,24 +87,25 @@ class footer(create):
         self.soup.find(id='copyright').string = copy_right
         self.to_html(str(self.soup.prettify()))
 
-p = photo()
-p.change_image(1,'img/coffee2.jpg','img/coffee2.jpg')
-# b = banner()
-# b.change_text(slide=1,line=2,text='fresh porto coffee')
-# # ví dụ
-# ft = footer()
-# ft.change_image('img/logo-light.png')
-# ft.change_address(1,'123','New York Street','New York','(123) 456-7890')
-# ft.change_copyright('© copyright 2021. All Rights Reserved.')
+# Example test 
+# p = photo()
+# p.change_image(1,'img/coffee2.jpg','img/coffee2.jpg')
+# # b = banner()
+# # b.change_text(slide=1,line=2,text='fresh porto coffee')
+# # # ví dụ
+# # ft = footer()
+# # ft.change_image('img/logo-light.png')
+# # ft.change_address(1,'123','New York Street','New York','(123) 456-7890')
+# # ft.change_copyright('© copyright 2021. All Rights Reserved.')
 
-# # ví dụ
-# new_items = ['new home','new about','reservations','menu','news','location','elements','buy porto!']
-# b = header()
-# b.change_items(new_items)
-# b.change_logo('img/logo2.png')
+# # # ví dụ
+# # new_items = ['new home','new about','reservations','menu','news','location','elements','buy porto!']
+# # b = header()
+# # b.change_items(new_items)
+# # b.change_logo('img/logo2.png')
 
 
-a = New()
-a.change_button('Xem them di')
+# a = New()
+# a.change_button('Xem them di')
 
-a.change_main('New home112321321','la mot ngoi nha moi')
+# a.change_main('New home112321321','la mot ngoi nha moi')
